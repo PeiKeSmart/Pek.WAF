c1aa06e2eb3e1977b50344c42da64eeb2b3ca0af
猿人易 <qcjxberin@outlook.com>
2024-11-11 18:19:09 +0800

    增加默认规则

--- Pek.WAF/WAFSetting.cs
+++ Pek.WAF/WAFSetting.cs
@@ -10,5 +10,20 @@ public class WAFSetting : Config<WAFSetting>
 {
     /// <summary>防火墙规则</summary>
     [Description("防火墙规则")]
-    public Rule? Ruleset { get; set; }
+    public Rule Ruleset { get; set; } = new Rule
+    {
+        Operator = "OrElse",
+        Rules = [
+        new Rule{
+            MemberName = "Path",
+            Operator = "EndsWidth",
+            Inputs = [".php"]
+        },
+        new Rule{
+            MemberName = "UserAgent",
+            Operator = "IsMatch",
+            TargetValue = "^(curl|java|python)"
+        }
+        ]
+    };
 }
