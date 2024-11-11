f6317b8117fc1c0aca1e989bfdfd91f011e5b008
猿人易 <qcjxberin@outlook.com>
2024-11-11 17:45:59 +0800

    WAF防火墙配置

---
+++ Pek.WAF/WAFSetting.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace Pek.WAF;
+
+/// <summary>WAF防火墙配置</summary>
+[Config("WAF")]
+public class WAFSetting : Config<WAFSetting>
+{
+    /// <summary>防火墙规则</summary>
+    [Description("防火墙规则")]
+    public Rule? Ruleset { get; set; }
+}
